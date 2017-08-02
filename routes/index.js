fbb545d91e096dac0b89a3177374d9c8ef228113
382767553@qq.com <382767553@qq.com>
2017-08-02 14:21:13 +0800

    222

--- routes/index.js
+++ routes/index.js
@@ -22,14 +22,15 @@ router.get('/export', function(req, res, next) {
     conf.cols = [
         {caption:'名称', type:'string'},
         {caption:'手机号', type:'string', width:'20' },
-        {caption:'年龄', type:'int'},
+        {caption:'年龄', type:'number'},
         {caption:'工作单位', type:'string', width:'45' },
         {caption:'职务', type:'string'},
         {caption:'单位电话', type:'string', width:'20'},
-   //     {caption:'芝麻信用分', type:'int'},
-    //    {caption:'借呗额度', type:'int'},
-        {caption:'信用卡额度', type:'int'},
-        {caption:'QQ', type:'int', width:'20'},
+         {caption:'芝麻信用分', type:'number'},
+         {caption:'借呗额度', type:'number'},
+        {caption:'信用卡额度', type:'number'},
+        {caption:'QQ', type:'number', width:'20'},
+		 {caption:'微信号', type:'string', width:'20'},
 	{caption:'录入时间', type:'string', width:'20'}
     ];
 
@@ -45,17 +46,18 @@ router.get('/export', function(req, res, next) {
             newrow.push(o.work);
             newrow.push(o.position);
             newrow.push(o.workmobile);
-       //     newrow.push(o.zmnum);
-      //      newrow.push(o.jbamount);
+            newrow.push(o.zmnum);
+             newrow.push(o.jbamount);
             newrow.push(o.cardamount);
             newrow.push(o.qq);
+			  newrow.push(o.wx);
             newrow.push(o.create_time);
             conf.rows.push(newrow);
         }
 	console.log(conf.rows)
         var result = nodeExcel.execute(conf);
         res.setHeader('Content-Type', 'application/vnd.openxmlformats');
-        res.setHeader("Content-Disposition", "attachment; filename=" + "客户资料.xlsx");
+        res.setHeader("Content-Disposition", "attachment; filename=" + "khzl.xlsx");
         res.end(result, 'binary');
     }).catch(function (err) {
         console.log("!"+err.message+"!")
